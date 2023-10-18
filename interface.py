e24026d129713c2b8f15f1b0bb14bbaec6db7d16
João Pedro <pcjoaopedro40@gmail.com>
2023-10-18 06:35:52 -0300

    Adicionei guias

--- interface.py
+++ interface.py
@@ -4,10 +4,25 @@ class App(ft.UserControl):
     def __init__(self):
         super().__init__()
 
-    
-    def build(self):
-        return ft.Column(
-            controls=[
-                ft.Text('Hello')
+        self.guias = ft.Tabs(
+            tabs=[
+                ft.Tab(
+                    text='Produtos'
+                ),
+                ft.Tab(
+                    text='Vendas'
+                )
             ]
         )
+    
+
+    def build(self):
+        return ft.Container(
+            ft.Column(
+                controls=[
+                    self.guias
+                ]
+            ),
+            alignment=ft.alignment.center
+        )
+        
